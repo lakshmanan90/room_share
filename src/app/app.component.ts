642f75f536be7443d868aab74b077cf0a177a3ea
lakshmanan90 <deepa.hariharan@mindzen.com>
2020-05-18 18:42:05 +0530

    changes done ny lakshman

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome Mr. Lakshman11';
+  title = 'Welcome Mr. Lakshm';
 }
